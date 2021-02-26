76c545612ae3f65157e12ed8a2d63ece746371c5
Cody_Jones <cbjones93@gmail.com>
2021-02-26 10:54:05 -0600

    tips done

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,18 @@
 console.log("main is loaded");
 
 
-import { FishList } from "./FishList.js";
+import { FishList } from "./fish/FishList.js";
 
 FishList();
 
+
+import { getTip } from './tips/TipData.js';
+
+const allTheTip = getTip()
+
+for (const tip of allTheTip) {
+    console.log(tip)
+}
+
+import {TipList} from "./tips/TipList.js"
+TipList();
